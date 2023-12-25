f30afd8841c3c8261619f3647fee130b12525493
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-12-25 22:11:48 +0000

    Added separate functions for videos and channels

--- src/main/java/youtube/controlpanel/model/LinkParser.java
+++ src/main/java/youtube/controlpanel/model/LinkParser.java
@@ -12,13 +12,31 @@ public class LinkParser {
      * @return The extracted video ID or null if not found.
      */
     public static String extractVideoIdFromUrl(String youtubeUrl) {
-        Pattern pattern = Pattern.compile(
-                "^https?://www\\.youtube\\.com/.*v=([^&]*)", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(youtubeUrl);
+        Pattern videoPattern = Pattern.compile(
+                "^https?://www\\.youtube\\.com/watch\\?v=([^&]*)", Pattern.CASE_INSENSITIVE);
+        Matcher videoMatcher = videoPattern.matcher(youtubeUrl);
 
-        if (matcher.find()) {
-            return matcher.group(1);
+        if (videoMatcher.find()) {
+            return videoMatcher.group(1);
+        }
+        return null;
+    }
+
+    /**
+     * Extracts the channel ID from a YouTube URL.
+     *
+     * @param youtubeUrl The full YouTube URL.
+     * @return The extracted channel ID or null if not found.
+     */
+    public static String extractChannelIdFromUrl(String youtubeUrl) {
+        Pattern channelPattern = Pattern.compile(
+                "^https?://www\\.youtube\\.com/channel/([^&/?]*)", Pattern.CASE_INSENSITIVE);
+        Matcher channelMatcher = channelPattern.matcher(youtubeUrl);
+
+        if (channelMatcher.find()) {
+            return channelMatcher.group(1);
         }
         return null;
     }
 }
+
