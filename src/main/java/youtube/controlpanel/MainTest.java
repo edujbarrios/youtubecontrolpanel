11af8f0e17120e0a5d65e7bd15e41f95d2543458
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-12-26 21:49:26 +0000

    Added a function to extract videos per year

--- src/main/java/youtube/controlpanel/MainTest.java
+++ src/main/java/youtube/controlpanel/MainTest.java
@@ -4,19 +4,20 @@ import com.google.api.services.youtube.model.Video;
 import youtube.controlpanel.model.data.*;
 import youtube.controlpanel.model.resources.LinkParser;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
- * Main application class for processing YouTube URLs.
+ * Main application class for processing YouTube URLs with extended video information.
  */
 public class MainTest {
 
     /**
-     * The entry point of the application.
+     * Entry point of the application.
+     * Retrieves and displays extended information about YouTube videos and channels.
      *
-     * @param args The command-line arguments.
+     * @param args Command-line arguments (not used).
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -45,16 +46,55 @@ public class MainTest {
                 continue;
             }
 
-            for (Video video : videosList) {
-                System.out.println("Video Title: " + video.getSnippet().getTitle());
-                System.out.println("Video Likes: " + video.getStatistics().getLikeCount());
-                System.out.println("Video Views: " + video.getStatistics().getViewCount());
-                System.out.println("Video Comments: " + video.getStatistics().getCommentCount());
-                System.out.println("-----");
-            }
+            displayVideoDetails(videosList);
         }
 
         scanner.close();
         System.out.println("Program terminated.");
+    }
+
+    /**
+     * Displays detailed information about a list of videos.
+     *
+     * @param videosList A list of Video objects.
+     */
+    private static void displayVideoDetails(List<Video> videosList) {
+        for (Video video : videosList) {
+            System.out.println("Video Title: " + video.getSnippet().getTitle());
+            System.out.println("Channel: " + video.getSnippet().getChannelTitle());
+            System.out.println("Published Date: " + video.getSnippet().getPublishedAt());
+            System.out.println("Likes: " + video.getStatistics().getLikeCount());
+            System.out.println("Views: " + video.getStatistics().getViewCount());
+            System.out.println("Comments: " + video.getStatistics().getCommentCount());
+            // Add more fields as needed, depending on what is available from the API
+            System.out.println("-----");
+        }
+    }
+
+    /**
+     * Displays the count of videos published per year for a list of videos.
+     *
+     * @param videosList A list of Video objects.
+     */
+    private static void displayVideosPerYear(List<Video> videosList) {
+        Map<Integer, Integer> videosPerYear = new HashMap<>();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+        for (Video video : videosList) {
+            try {
+                Date publishedDate = dateFormat.parse(video.getSnippet().getPublishedAt().toStringRfc3339());
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(publishedDate);
+                int year = calendar.get(Calendar.YEAR);
+
+                videosPerYear.put(year, videosPerYear.getOrDefault(year, 0) + 1);
+            } catch (ParseException e) {
+                System.out.println("Error parsing date: " + e.getMessage());
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : videosPerYear.entrySet()) {
+            System.out.println("Year " + entry.getKey() + ": " + entry.getValue() + " video(s)");
+        }
     }
 }
