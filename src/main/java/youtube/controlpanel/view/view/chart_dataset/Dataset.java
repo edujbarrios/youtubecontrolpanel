2ef77ef7320247f370a0fdbdcc55911f9a603de0
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2024-01-03 14:32:36 +0000

    Fixing issue

--- src/main/java/youtube/controlpanel/view/view/chart_dataset/Dataset.java
+++
@@ -1,8 +0,0 @@
-package youtube.controlpanel.view.chart_dataset;
-
-import com.google.api.services.youtube.model.Video;
-import org.jfree.data.category.DefaultCategoryDataset;
-
-public interface Dataset {
-    public DefaultCategoryDataset createDataset(Video video);
-}
--- src/main/java/youtube/controlpanel/view/view/observer/YouTubeDataObserver.java
+++
@@ -1,8 +0,0 @@
-package youtube.controlpanel.view.observer;
-
-import com.google.api.services.youtube.model.Video;
-import java.util.List;
-
-public interface YouTubeDataObserver {
-    void update(List<Video> videos);
-}
