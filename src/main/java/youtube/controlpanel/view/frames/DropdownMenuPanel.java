c03680eb2302b0097217d2c0b66cf003b4702ed7
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2024-01-07 12:03:31 +0000

    Addpated to use controllers for the buttons

--- src/main/java/youtube/controlpanel/view/frames/DropdownMenuPanel.java
+++ src/main/java/youtube/controlpanel/view/frames/DropdownMenuPanel.java
@@ -2,7 +2,6 @@ package youtube.controlpanel.view.frames;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 
 public class DropdownMenuPanel extends JPanel {
     ControlPanelView controlPanelView;
@@ -12,40 +11,24 @@ public class DropdownMenuPanel extends JPanel {
     DropdownMenuPanel(ControlPanelView controlPanelView) {
         this.controlPanelView = controlPanelView;
 
-        // Create combo box with chart options
+        // Crear combo box con opciones de gráfico
         String[] chartOptions = {"BarChart Graph", "PieChart Graph", "AreaChart Graph", "RingChart Graph", "WaterfallChart Graph", "LineChart Graph"};
-        chartComboBox = new JComboBox<>(chartOptions);
-        styleComboBox(chartComboBox); // Apply style to JComboBox
+        chartComboBox = ComboBoxFactory.createStyledComboBox(chartOptions);
 
-        // Create button to apply selection
-        applyButton = new JButton("Apply Selection");
-        styleButton(applyButton); // Apply style to JButton
+        // Crear botón para aplicar la selección
+        applyButton = ButtonFactory.createButton("Apply Selection");
 
         CheckboxPanel checkboxPanel = new CheckboxPanel();
-        // Add action listener to apply button
+        // Añadir ActionListener al botón de aplicar
         applyButton.addActionListener(e -> {
             String selectedChart = (String) chartComboBox.getSelectedItem();
             controlPanelView.displayChart(selectedChart, checkboxPanel.getCheckboxes());
         });
 
-        // Set layout and add components
+        // Establecer el layout y añadir componentes
         setLayout(new GridLayout(3, 1));
         add(chartComboBox);
         add(checkboxPanel);
         add(applyButton);
-    }
-
-    private void styleComboBox(JComboBox<String> comboBox) {
-        comboBox.setBackground(new Color(255, 255, 255)); // Same background as fetchButton
-        comboBox.setForeground(new Color(205, 32, 31)); // Changed to Youtube RED
-        comboBox.setFont(new Font("Arial", Font.PLAIN, 14)); // Same font as fetchButton
-    }
-
-    private void styleButton(JButton button) {
-        button.setBackground(new Color(205, 32, 31)); // Same background as fetchButton
-        button.setForeground(Color.BLACK); // Same foreground as fetchButton
-        button.setFont(new Font("Arial", Font.PLAIN, 14)); // Same font as fetchButton
-        button.setBorder(BorderFactory.createRaisedBevelBorder()); // Same border as fetchButton
-        button.setFocusPainted(false);
     }
 }
